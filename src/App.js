01480eecb7554bdc4bc7130f3ff8426628aa6945
surajrocks1997 <surajrocks1997@gmail.com>
2020-09-23 16:28:23 +0530

    collection preview added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Homepage from './pages/homepage/homepage.component';
 import './App.css';
+import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop-componenet';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
+        <Route  path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
