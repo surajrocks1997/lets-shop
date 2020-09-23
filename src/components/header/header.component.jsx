1869e5d938f2390f5682b311034d143ee698e9b6
surajrocks1997 <surajrocks1997@gmail.com>
2020-09-23 17:22:29 +0530

    padding corrected throught the webapp, homepage link added

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -11,6 +11,9 @@ const Header = () => {
         <Logo className='logo' />
       </Link>
       <div className='options'>
+      <Link className='option' to='/' >
+          HOME
+        </Link>
         <Link className='option' to='/shop' >
           SHOP
         </Link>
