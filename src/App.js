b477863d55a3d85f2d94ec7d230aaba7e0e576ac
surajrocks1997 <surajrocks1997@gmail.com>
2020-09-23 13:52:18 +0530

    homepage and directory component added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 
-import Homepage from './homepage.component'
+import Homepage from './pages/homepage/homepage.component';
 
 function App() {
-  return <div>
-   <Homepage />
-  </div>;
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
 }
 
 export default App;
