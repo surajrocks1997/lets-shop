6855cbe8840490f94b837b38fc89fbc37c8b3d4c
surajrocks1997 <surajrocks1997@gmail.com>
2020-09-24 20:16:52 +0530

    signup component added - UI, signup using email,password working

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 import './auth.styles.scss';
 
 const Auth = () => {
-  return <div className='auth'>
-    <SignIn />
-  </div>;
+  return (
+    <div className='auth'>
+      <SignIn />
+      <SignUp />
+    </div>
+  );
 };
 
 export default Auth;
